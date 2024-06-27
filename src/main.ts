28a8f2e99af0185045d14e0172dec3f5eb514099
Ahsan <ahsanaz461@gmail.com>
2024-06-27 08:56:57 +0700

    chore: Add initial http exception filter and bootstrapper

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { HttpExceptionFilter } from './http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+
+const CORS_URLS = ['http://localhost:3000'];
+const PORT = 2000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+  app.enableCors({ origin: CORS_URLS });
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(2000);
 }
 bootstrap();
