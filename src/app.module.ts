a9d7ba1f3620fd7ab6575af95f820f19d489e60e
Ahsan <ahsanaz461@gmail.com>
2024-06-28 16:19:02 +0700

    Lint fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './core/auth/auth.module';
-import { UserModule } from './core/user/user.module';
 import { TransactionModule } from './core/transaction/transaction.module';
+import { UserModule } from './core/user/user.module';
 
 @Module({
   imports: [
